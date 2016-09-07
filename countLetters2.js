3549cf156cfc78d22a5f44730fa9d5958bc36725
devin-web <devin@alumni.sfu.ca>
2016-09-07 00:44:25 +0000

    initial commit

---
+++ countLetters2.js
@@ -0,0 +1,41 @@
+cmdlineArgs = process.argv.slice(2);
+
+if(cmdlineArgs.length === 0 ){
+ console.log("Error: no input string" );
+}
+else{
+  console.log( countLetters( cmdlineArgs[0].toLowerCase() ) );
+}
+
+// function countLetters( curString ){
+//   curString = curString.split("");
+//   var myObj = {};
+//   for( char in curString ){
+//     if( curString[char] !== " " ){
+//       if( myObj.hasOwnProperty(curString[char]) ){
+//         myObj[curString[char]]++;
+//       }
+//       else {
+//         myObj[curString[char]] = 1;
+//       }
+//     }
+//   }
+//   return myObj;
+// }
+
+function countLetters( curString ){
+  curString = curString.split("");
+  var myObj = {};
+  for( char in curString ){
+    if( curString[char] !== " " ){
+      if( myObj.hasOwnProperty( curString[char] ) ){
+        myObj[curString[char]].count++;
+        myObj[curString[char]].indicies.push(char);
+      }
+      else {
+        myObj[curString[char]]  = { count: 1, indicies: [char] };
+      }
+    }
+  }
+  return myObj;
+}
